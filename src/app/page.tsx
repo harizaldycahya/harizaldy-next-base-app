a704327318e2562f4ba201e04e0dab72e361239d
harizaldycahya <zaldypratama82@gmail.com>
2025-08-17 11:07:45 +0700

    installing next, prisma, and tailwind

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="grid gap-2 grid-cols-1 sm:grid-cols-1 lg:grid-cols-2 xl:grid-cols-3">
+        <div className="bg-amber-200  h-40"></div>
+        <div className="bg-amber-300  h-40"></div>
+        <div className="bg-amber-400  h-40"></div>
+    </div>
+  );
+}
